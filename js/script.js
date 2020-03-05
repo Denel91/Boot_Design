44c1d3f3e47f102a94c21ac7c91fe039337390fe
Daniele Di Salvo <disi91@hotmail.it>
2020-03-05 21:48:01 +0100

    INSERT a js function

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+    $("#button-1").on("click", function () {
+        $('#myNav .nav-third').tab('show');
+    });
+
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+        console.log("[show.bs.tab] Tab da visualizzare tra poco: " + $(e.target).html());
+        console.log("[show.bs.tab] Tab ora visibile: " + $(e.relatedTarget).html());
+    })
+});
